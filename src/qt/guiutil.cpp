6f93a347ee0ccb4decf1a87dd5e03eed45464fc1
nathan <nathan.senn90@gmail.com>
2020-09-06 18:33:38 +0800

    GUI Address placeholder

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -110,7 +110,7 @@ void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a Helix address (e.g. %1)").arg("PCYiHgGJJ6xGHqivmdZrYjRnhaYf6AJ2Mp"));
+    widget->setPlaceholderText(QObject::tr("Enter a Helix address (e.g. %1)").arg("HJ8StaZntNKrvoheVqCcGgUpe7qoxE6dVb"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
